e4abb9ada92a87430898a2a1edfd7aa5ab116da7
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2020-06-22 21:02:44 +0900

    [feat]auth0の接続情報を追加

---
+++ src/config/const.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'auth0' => [
+        'domain' => env('AUTH0_DOMAIN', ''),
+        'client_id' => env('AUTH0_CLIENT_ID', ''),
+        'client_secret' => env('AUTH0_CLIENT_SECRET', ''),
+    ]
+];
